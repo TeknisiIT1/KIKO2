26bd68544da463aad4a8909e03cd81963c68c15a
TeknisiIT1 <139561646+TeknisiIT1@users.noreply.github.com>
2023-11-26 23:33:33 +0700

    Update xolpanel.sh

--- xolpanel.sh
+++ xolpanel.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
-
 #install
+rm -rf xolpanel.sh
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/valkry/xolpanel.git
+git clone https://github.com/marlina697/xolpanel.git
 unzip xolpanel/xolpanel.zip
 pip3 install -r xolpanel/requirements.txt
 pip3 install pillow
-
 #isi data
-echo ""
-read -e -p "[*] Input your Bot Token : " bottoken
-read -e -p "[*] Input Your Id Telegram :" admin
-read -e -p "[*] Input Your Domain :" domain
+grenbo="\e[92;1m"
+NC='\033[0m'
+clear
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│$NC\033[42m          ADD TO BOT TELE                  $NC"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+read -e -p "[*] Input Bot Token   :  " bottoken
+read -e -p "[*] Input Id Tele     :  " admin
+read -e -p "[*] Input domain      :  " domain
+read -e -p "[*] Input NSdomain    :  " sldomain
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e ""
+echo -e ""
 echo -e BOT_TOKEN='"'$bottoken'"' >> /root/xolpanel/var.txt
 echo -e ADMIN='"'$admin'"' >> /root/xolpanel/var.txt
 echo -e DOMAIN='"'$domain'"' >> /root/xolpanel/var.txt
+echo -e SLDOMAIN='"'$sldomain'"' >> /root/xolpanel/var.txt
 clear
-echo "Done"
-echo "Your Data Bot"
-echo -e "==============================="
-echo "DOMAIN         : $bottoken"
-echo "Email          : $admin"
-echo "Api Key        : $domain"
-echo -e "==============================="
-echo "Setting done"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│$NC\033[42m          DATA YOUR BOT                  $NC"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│  ${grenbo}[NOTE]${NC} \033[0;36mTOKEN BOT     : $bottoken ${NC}"
+echo -e "\033[1;93m│  ${grenbo}[NOTE]${NC} \033[0;36mID TELE       : $admin ${NC}"
+echo -e "\033[1;93m│  ${grenbo}[NOTE]${NC} \033[0;36mDOMAIN        : $domainl ${NC}"
+echo -e "\033[1;93m│  ${grenbo}[NOTE]${NC} \033[0;36mNSDomain      : $sldomain ${NC}"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo "Setting done Please wait 10s"
+sleep 10
 
 cat > /etc/systemd/system/xolpanel.service << END
 [Unit]
@@ -44,5 +57,25 @@ systemctl start xolpanel
 systemctl enable xolpanel
 
 clear
-
-echo " Installations complete, type /menu on your bot"
+grenbo="\e[92;1m"
+NC='\033[0m'
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│$NC\033[42m          INSTALLATION COMPLETED              $NC"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+sleep 2
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│$NC\033[42m          DATA YOUR BOT                  $NC"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│  ${grenbo}[NOTE]${NC} \033[0;36mTOKEN BOT : $bottoken ${NC}"
+echo -e "\033[1;93m│  ${grenbo}[NOTE]${NC} \033[0;36mID TELE   : $admin ${NC}"
+echo -e "\033[1;93m│  ${grenbo}[NOTE]${NC} \033[0;36mDOMAIN    : $domainl ${NC}"
+echo -e "\033[1;93m│  ${grenbo}[NOTE]${NC} \033[0;36mNSDomain  : $sldomain ${NC}"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│  ${grenbo}[NOTE]${NC} \033[0;36mType, /menu On Your Bot Tele${NC}"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e ""
+# read -n 1 -s -r -p "Press any key to Reboot"
+rm -rf xolpanel.sh
+clear
